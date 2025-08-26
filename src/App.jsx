64179b063a8598a6ed56814816e25fcddd4f8092
JvEztraaaa <180340662+JvEztraaaa@users.noreply.github.com>
2025-08-27 04:30:45 +0800

    feat: imported routes and page setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import MapPage from './pages/MapPage';
 import ContactUsPage from './pages/ContactUsPage';
+import FuelCalculatorPage from './pages/FuelCalculatorPage';
+import MyTripsPage from './pages/MyTripsPage';
+import RefuelHistoryPage from './pages/RefuelHistoryPage';
+import StatisticsPage from './pages/StatisticsPage';
 import './App.css';
 
 export default function App() {
@@ -19,8 +23,12 @@ export default function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/map" element={<MapPage />} />
-            <Route path="/contact" element={<ContactUsPage />} />
             <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactUsPage />} />
+            <Route path="/fuel-calculator" element={<FuelCalculatorPage />} />
+            <Route path="/my-trips" element={<MyTripsPage />} />
+            <Route path="/refuel-history" element={<RefuelHistoryPage />} />
+            <Route path="/statistics" element={<StatisticsPage />} />
           </Routes>
         </SidePanelProvider>
       </Router>
