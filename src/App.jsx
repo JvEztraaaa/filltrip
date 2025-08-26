b36bd77e96349ee188ddd90bd58e10d15e907ab6
JvEztraaaa <180340662+JvEztraaaa@users.noreply.github.com>
2025-08-27 02:19:17 +0800

    feat: added route for other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { SidePanelProvider } from './components/SidePanel';
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
 import LoginPage from './pages/LoginPage';
@@ -12,14 +13,16 @@ export default function App() {
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/map" element={<MapPage />} />
-          <Route path="/contact" element={<ContactUsPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
+        <SidePanelProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/map" element={<MapPage />} />
+            <Route path="/contact" element={<ContactUsPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </SidePanelProvider>
       </Router>
     </AuthProvider>
   );
